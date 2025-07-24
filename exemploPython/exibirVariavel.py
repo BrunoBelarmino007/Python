89861d7d9763c73f84c8bffa88c5b354578d3f9b
Bruno Belarmino da Silva <brunobelarmino336@outlook.com>
2025-07-24 15:17:12 -0300

    Foi adicionado no exibirVariavel tipos de entrada de dados

--- exemploPython/exibirVariavel.py
+++ exemploPython/exibirVariavel.py
@@ -1,6 +1,17 @@
-codigo = 105
-salario = 1650.00
-nome = "Bruno"
+# codigo = 105
+# salario = 1650.00
+# nome = "Bruno"
+# ativo = True
+
+# print("Codigo: %d" % codigo)
+# print("Nome: %s" % nome)
+# print("Salario: %.2f" % salario)
+# print("Ativo: %s" % ativo)
+
+
+codigo = int(input("Digite o código do Funcionário: "))
+nome = input("Digite o nome do Funcionário: ")
+salario = float(input("Informe o salário: "))
 ativo = True
 
 print("Codigo: %d" % codigo)
