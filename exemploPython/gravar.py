661a7ce9cecf7b4a75150cb58d8c97eddaab522d
Bruno Belarmino da Silva <brunobelarmino336@outlook.com>
2025-07-24 17:20:54 -0300

    Foi adicionado a classe gravar junto com o arquivo de texto a ser manipulado

---
+++ exemploPython/gravar.py
@@ -0,0 +1,10 @@
+arquivo = open('arqText.txt', 'w')
+
+arquivo.write('Curso Python\n')
+arquivo.write('Aula Prática\n')
+arquivo.close()
+
+#Leitura do arquivo de texto
+leitura = open('arqText.txt', 'r')
+print(leitura.read())
+leitura.close()
