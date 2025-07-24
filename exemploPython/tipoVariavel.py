e8049b95cd386b745b9822179631d38accb03c14
Bruno Belarmino da Silva <brunobelarmino336@outlook.com>
2025-07-24 15:02:42 -0300

    Foi adicionado outra forma de concatenar os tipos de variavel

--- exemploPython/tipoVariavel.py
+++ exemploPython/tipoVariavel.py
@@ -9,3 +9,4 @@ situacao = True
 # print(tipo)
 
 print("Código:", codigo, "Nome:", nome, "O salário atual é de:", salario)
+print("Código: "+ str(codigo)+ "; Nome: "+ str(nome)+ "; O salário atual é de: "+ str(salario))
