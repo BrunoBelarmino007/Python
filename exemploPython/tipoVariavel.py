7af60d4df0be43f60a6e716cd9b3bee5aa7d92e1
Bruno Belarmino da Silva <brunobelarmino336@outlook.com>
2025-07-24 14:57:57 -0300

    Foi adicionado concatenização nos tipos de variavel

--- exemploPython/tipoVariavel.py
+++ exemploPython/tipoVariavel.py
@@ -3,7 +3,9 @@ salario = 1500.00
 nome = "José"
 situacao = True
 
-tipo = type(salario)
+# tipo = type(salario)
 
-print(salario)
-print(tipo)
+# print(salario)
+# print(tipo)
+
+print("Código:", codigo, "Nome:", nome, "O salário atual é de:", salario)
